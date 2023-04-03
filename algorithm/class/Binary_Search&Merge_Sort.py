a699d58fd57cb25b370a790569796e0d83a57766
황준태 <88199966+hwangjuntae@users.noreply.github.com>
2023-04-03 14:44:00 +0900

    4/3 ing

---
+++ algorithm/class/Binary_Search&Merge_Sort.py
@@ -0,0 +1,64 @@
+def binary_search(arr, target):
+
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+def merge_sort(arr):
+
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result += left[i:]
+    result += right[j:]
+
+    return result
+
+import random
+
+def create_random_array(size, minimum, maximum):
+    return [random.randint(minimum, maximum) for _ in range(size)]
+
+a = create_random_array(10, 0, 10)
+
+print(a)
+
+list = merge_sort(a)
+
+print(list)
+
+print(binary_search(list, a[4]))
+
+
+
